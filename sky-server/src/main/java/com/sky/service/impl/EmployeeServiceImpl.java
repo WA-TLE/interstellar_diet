1d80fd86dbf1902ba410671006bb10c54791cd47
dy <dyu32345@gmail.com>
2023-08-16 21:14:44 +0800

    添加新增员工功能
    添加新增员工功能, 并且对新增员工的功能进行完善
    用户名重复处理异常
    添加处理人 id

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,8 @@ package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -54,6 +59,39 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+
+        //  对象属性拷贝
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        //  设置其他状态
+
+        //  账号状态
+        employee.setStatus(StatusConstant.ENABLE);
+
+        //  设置默认密码 123456, 后续员工可以自己更改
+        //  使用 md5 加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        //  设置当前记录的创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //  设置当前记录创建人和修改人的 id
+        //  获取当前记录人的 id
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+
+        //  持久层
+        employeeMapper.insert(employee);
+
+
+
     }
 
 }
