ead9d245c2d5d9960c8babaab36b360f3e0e4539
dy <dyu32345@gmail.com>
2023-09-07 15:59:19 +0800

    订单支付功能

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -24,4 +24,12 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    /**
+     * 根据用户 id 查询相应用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
